2d943bdfdd0f05bfc238a692b30724dd76cabfff
gckopper <41166074+gckopper@users.noreply.github.com>
2022-02-16 01:01:55 -0300

    added test for a cookie jar

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"pas/auth"
 	"testing"
@@ -73,29 +74,63 @@ func TestServer(t *testing.T) {
 	}
 }
 
+// TestServer tests the authHandlerFunc directly with the provided example user
+func TestServerCookie(t *testing.T) {
+	server := httptest.NewTLSServer(http.HandlerFunc(authHandlerFunc)) // Uses a test server
+	defer server.Close()
+	request, err := http.NewRequest("GET", server.URL, nil) // Creates a request to the test server
+	if err != nil {
+		t.Fatal(err)
+	}
+	request.Header = map[string][]string{ // Prepare an authentication header
+		"Username": {"user"},
+		"Password": {"a"},
+		"OTP": {fmt.Sprint(auth.Totp(
+			"IL6V2C3SBR7G6HIEFJOGEZFMPLDLXO7W7E4GJILPRFBIC5HXN7NNED5IRN67LDJNCI3JLAW4RCJKR5CKSMMGT7GL4O3D3GSMSXWCLZY=",
+			uint64(time.Now().Unix()/30)))},
+	}
+	jar, err := cookiejar.New(nil)
+	server.Client().Jar = jar
+	if err != nil {
+		t.Fatal(err)
+	}
+	response, err := server.Client().Do(request) // Make the request
+	if err != nil {
+		t.Fatal(err)
+	}
+	if response.StatusCode != 200 { // If the authentication fails then so does the test
+		t.Fatal(response.StatusCode)
+	}
+	response, err = server.Client().Get(server.URL) // Make the request
+	if err != nil {
+		t.Fatal(err)
+	}
+	if response.StatusCode != 200 { // If the authentication fails then so does the test
+		t.Fatal(response.StatusCode)
+	}
+}
+
 func TestSampleServer(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(authHandlerFunc)) // Uses a test server
 	defer server.Close()
-	for i := 0; i < 100; i++ {
-		request, err := http.NewRequest("GET", server.URL, nil) // Creates a request to the test server
-		if err != nil {
-			t.Fatal(err)
-		}
-		request.Header = map[string][]string{ // Prepare an authentication header
-			"Username": {"user"},
-			"Password": {"a"},
-			"OTP": {fmt.Sprint(auth.Totp(
-				"IL6V2C3SBR7G6HIEFJOGEZFMPLDLXO7W7E4GJILPRFBIC5HXN7NNED5IRN67LDJNCI3JLAW4RCJKR5CKSMMGT7GL4O3D3GSMSXWCLZY=",
-				uint64(time.Now().Unix()/30)))},
-		}
-		time.Sleep(2)
-		response, err := server.Client().Do(request) // Make the request
-		if err != nil {
-			t.Fatal(err)
-		}
-		if response.StatusCode != 200 { // If the authentication fails then so does the test
-			t.Fatal(response.StatusCode)
-		}
+	request, err := http.NewRequest("GET", server.URL, nil) // Creates a request to the test server
+	if err != nil {
+		t.Fatal(err)
+	}
+	request.Header = map[string][]string{ // Prepare an authentication header
+		"Username": {"user"},
+		"Password": {"a"},
+		"OTP": {fmt.Sprint(auth.Totp(
+			"IL6V2C3SBR7G6HIEFJOGEZFMPLDLXO7W7E4GJILPRFBIC5HXN7NNED5IRN67LDJNCI3JLAW4RCJKR5CKSMMGT7GL4O3D3GSMSXWCLZY=",
+			uint64(time.Now().Unix()/30)))},
+	}
+	time.Sleep(2)
+	response, err := server.Client().Do(request) // Make the request
+	if err != nil {
+		t.Fatal(err)
+	}
+	if response.StatusCode != 200 { // If the authentication fails then so does the test
+		t.Fatal(response.StatusCode)
 	}
 }
 
