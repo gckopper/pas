c478cca5897a5526f65b5528da861f14c2021d29
Gabriel-Kopper <gabrielkopper@sesiom.com.br>
2022-10-14 09:41:12 -0300

    better error handling for cookies; refactoring

--- main.go
+++ main.go
@@ -37,25 +37,36 @@ func main() {
 }
 func authHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	userCookie, err := r.Cookie("SessionCookie") // Try to grab the cookie named SessionCookie
-	if err == nil {
-		cookie, exists := cookieList[userCookie.Value]            // Will get the cookie from the cookieList
-		if !exists || time.Now().Unix() > cookie.Expires.Unix() { // Making sure the cookie exists
-			// In case that the cookie expire we send it back with MaxAge = -1 to inform the browser
-			newCookie := http.Cookie{
-				Secure: true,
-				Name:   "SessionCookie",
-				Value:  userCookie.Value,
-				MaxAge: -1,
-			}
-			delete(cookieList, userCookie.Value) // Delete the cookie from the cookieList
-			http.SetCookie(w, &newCookie)
-			w.Write([]byte("Cookie not found or expired")) // If the cookie does not exist, write this to the response
-			w.WriteHeader(403)
-			return
+	switch err {
+	case nil:
+		giveCookie(w, userCookie)
+	case http.ErrNoCookie:
+		checkCredentials(w, r, userCookie)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func giveCookie(w http.ResponseWriter, userCookie *http.Cookie) {
+	cookie, exists := cookieList[userCookie.Value]            // Will get the cookie from the cookieList
+	if !exists || time.Now().Unix() > cookie.Expires.Unix() { // Making sure the cookie exists
+		// In case that the cookie expire we send it back with MaxAge = -1 to inform the browser
+		newCookie := http.Cookie{
+			Secure: true,
+			Name:   "SessionCookie",
+			Value:  userCookie.Value,
+			MaxAge: -1,
 		}
-		w.WriteHeader(200) // If we have a valid unexpired cookie it's all good to go
+		delete(cookieList, userCookie.Value) // Delete the cookie from the cookieList
+		http.SetCookie(w, &newCookie)
+		w.Write([]byte("Cookie not found or expired")) // If the cookie does not exist, write this to the response
+		w.WriteHeader(403)
 		return
 	}
+	w.WriteHeader(http.StatusOK) // If we have a valid unexpired cookie it's all good to go
+}
+
+func checkCredentials(w http.ResponseWriter, r *http.Request, userCookie *http.Cookie) {
 	// Grab the authentication headers
 	username := r.Header.Get("Username")
 	password := r.Header.Get("Password")
@@ -66,7 +77,7 @@ func authHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	// return "" (empty string) if there is no header with such name
 	if (password == "") || (err != nil) || (username == "") || (len(otpString) > 6) || (len(username) > 64) || (len(password) > 64) {
 		log.Println(username, "is password invalid?", password == "" || len(password) > 64, err, otp, otpString)
-		w.WriteHeader(403)
+		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 	// Confirm the received valid credentials
@@ -84,10 +95,10 @@ func authHandlerFunc(w http.ResponseWriter, r *http.Request) {
 		// Add the new cookie to the front of the list as it is very likely to be used immediately
 		// By the very nature of lists things at the end take more time to reach
 		cookieList[newCookie.Value] = newCookie // Add the new cookie to the cookieList
-		w.WriteHeader(200)
+		w.WriteHeader(http.StatusOK)
 		return
 	}
 	// Returns Unauthorized for users with no cookie and no credentials
 	// Used by nginx to redirect the user to the login page
-	w.WriteHeader(401)
+	w.WriteHeader(http.StatusForbidden)
 }
